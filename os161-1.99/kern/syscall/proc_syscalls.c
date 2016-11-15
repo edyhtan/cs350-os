753b0fe5a18c8da7ab1a4ee34154b87120c3c338
yh2tan <yh2tan@uwaterloo.ca>
2016-11-14 22:39:13 -0500

    adding

--- os161-1.99/kern/syscall/proc_syscalls.c
+++ os161-1.99/kern/syscall/proc_syscalls.c
@@ -299,7 +299,7 @@ sys_execv(int *retval, userptr_t program, userptr_t args){
     //int argc = 0;
     
     // copy arguments and program name into the kernal
-    int argc = 0;
+    int argc;
     char **argcpy = copying_arg(program, args, &argc); // argc are expected to be at least 1
     
     if (argcpy == NULL){
